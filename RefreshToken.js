de031822032a72c5909f885de7d17a748602ebd9
Full Name <mohabelbasiry@sarri.sa>
2024-09-11 02:07:21 +0300

    'update'

--- RefreshToken.js
+++ RefreshToken.js
@@ -189,6 +189,31 @@ export const AuthProvider = ({ children }) => {
 
 export default AuthContext;
 `;
+const nextMiddleware =
+  `import { NextResponse } from "next/server";
+
+import { cookies } from "next/headers";
+
+export function middleware(request) {
+  const pathname = request.nextUrl.pathname;
+
+  const token = cookies().get("token")?.value;
+
+  const role = cookies().get("role")?.value;
+
+  if (pathname.includes("login") || pathname?.includes("signup")) {
+    if (token && token !== "" && role && role === "user") {
+      return NextResponse.redirect(new URL("/", request.nextUrl));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
+`;
 const useRefreshTokenContent = ` import axios from '.';
 import useAuth from './auth';
  
@@ -274,6 +299,207 @@ const ReadMe = `
   // }, []);
 
 `;
+
+const formValidation = `
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { signIn } from "next-auth/react";
+import toast from "react-hot-toast";
+import { Icon } from "@iconify/react";
+
+import googleIcon from "@/public/images/auth/google.png";
+import facebook from "@/public/images/auth/facebook.png";
+import twitter from "@/public/images/auth/twitter.png";
+import GithubIcon from "@/public/images/auth/github.png";
+
+const schema = z.object({
+  email: z.string().email({ message: "Your email is invalid." }),
+  password: z.string().min(4),
+});
+import { useMediaQuery } from "@/hooks/use-media-query";
+
+const LogInForm = () => {
+  const [isPending, startTransition] = React.useTransition();
+  const [passwordType, setPasswordType] = React.useState("password");
+  const isDesktop2xl = useMediaQuery("(max-width: 1530px)");
+
+  const togglePasswordType = () => {
+    setPasswordType((prevType) =>
+      prevType === "text" ? "password" : "text"
+    );
+  };
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+    mode: "all",
+    defaultValues: {
+      email: "dashtail@codeshaper.net",
+      password: "password",
+    },
+  });
+
+  const onSubmit = (data) => {
+    startTransition(async () => {
+      let response = await signIn("credentials", {
+        email: data.email,
+        password: data.password,
+        redirect: false,
+      });
+      if (response?.ok) {
+        toast.success("Login Successful");
+        window.location.assign("/dashboard");
+        reset();
+      } else if (response?.error) {
+        toast.error(response?.error);
+      }
+    });
+  };
+
+  return (
+    <div className="w-full py-10">
+      <Link href="/dashboard" className="inline-block">
+        <div className="h-10 w-10 2xl:w-14 2xl:h-14 bg-primary" />
+      </Link>
+      <div className="2xl:mt-8 mt-6 2xl:text-3xl text-2xl font-bold text-default-900">
+        Hey, Hello 👋
+      </div>
+      <div className="2xl:text-lg text-base text-default-600 2xl:mt-2 leading-6">
+        Enter the information you entered while registering.
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)} className="mt-5 2xl:mt-7">
+        <div>
+          <label htmlFor="email" className="mb-2 font-medium text-default-600">
+            Email
+          </label>
+          <input
+            disabled={isPending}
+            {...register("email")}
+            type="email"
+            id="email"
+            className={\`w-full px-3 py-2 border \${errors.email ? "border-red-500" : "border-gray-300"} rounded-md focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent\`}
+            size={!isDesktop2xl ? "xl" : "lg"}
+          />
+        </div>
+        {errors.email && (
+          <div className="text-red-500 mt-2">{errors.email.message}</div>
+        )}
+
+        <div className="mt-3.5">
+          <label htmlFor="password" className="mb-2 font-medium text-default-600">
+            Password
+          </label>
+          <div className="relative">
+            <input
+              disabled={isPending}
+              {...register("password")}
+              type={passwordType}
+              id="password"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
+              size={!isDesktop2xl ? "xl" : "lg"}
+              placeholder=" "
+            />
+            <div
+              className="absolute top-1/2 -translate-y-1/2 right-4 cursor-pointer"
+              onClick={togglePasswordType}
+            >
+              {passwordType === "password" ? (
+                <Icon icon="heroicons:eye" className="w-5 h-5 text-gray-400" />
+              ) : (
+                <Icon icon="heroicons:eye-slash" className="w-5 h-5 text-gray-400" />
+              )}
+            </div>
+          </div>
+        </div>
+        {errors.password && (
+          <div className="text-red-500 mt-2">{errors.password.message}</div>
+        )}
+
+        <div className="mt-5 mb-8 flex flex-wrap gap-2">
+          <div className="flex-1 flex items-center gap-1.5">
+            <input
+              type="checkbox"
+              className="border-gray-300 mt-[1px]"
+              id="isRemembered"
+            />
+            <label htmlFor="isRemembered" className="text-sm text-default-600 cursor-pointer">
+              Remember me
+            </label>
+          </div>
+          <Link href="/auth/forgot" className="flex-none text-sm text-primary">
+            Forget Password?
+          </Link>
+        </div>
+
+        <button
+          type="submit"
+          className={\`w-full py-2 \${isPending ? "bg-gray-400" : "bg-primary"} text-white rounded-md text-lg\`}
+          disabled={isPending}
+        >
+          {isPending ? (
+            <div className="flex items-center justify-center">
+              <Icon icon="lucide:loader" className="mr-2 h-4 w-4 animate-spin" />
+              Loading...
+            </div>
+          ) : (
+            "Sign In"
+          )}
+        </button>
+      </form>
+
+      <div className="mt-6 xl:mt-8 flex flex-wrap justify-center gap-4">
+        <button
+          type="button"
+          className="p-2 rounded-full border border-gray-300 hover:bg-transparent"
+          disabled={isPending}
+          onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
+        >
+          <img src={googleIcon} alt="google" className="w-5 h-5" />
+        </button>
+
+        <button
+          type="button"
+          className="p-2 rounded-full border border-gray-300 hover:bg-transparent"
+          disabled={isPending}
+          onClick={() => signIn("github", { callbackUrl: "/dashboard", redirect: false })}
+        >
+          <img src={GithubIcon} alt="github" className="w-5 h-5" />
+        </button>
+
+        <button
+          type="button"
+          className="p-2 rounded-full border border-gray-300 hover:bg-transparent"
+        >
+          <img src={facebook} alt="facebook" className="w-5 h-5" />
+        </button>
+
+        <button
+          type="button"
+          className="p-2 rounded-full border border-gray-300 hover:bg-transparent"
+        >
+          <img src={twitter} alt="twitter" className="w-5 h-5" />
+        </button>
+      </div>
+
+      <div className="mt-5 2xl:mt-8 text-center text-base text-gray-600">
+        Don't have an account?{" "}
+        <Link href="/auth/register" className="text-primary">
+          Sign Up
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LogInForm;
+`;
+
 function createFile(filePath, content) {
   fs.writeFileSync(filePath, content, { flag: "w" });
   console.log(`File created at: ${filePath}`);
@@ -323,6 +549,10 @@ const apiStructure = {
   },
   context: {
     "AuthProvider.js": `${AuthProvider}`,
+  },
+  next: {
+    "middleware.js": `${nextMiddleware}`,
+    "form.js": `${formValidation}`,
   },
   readMe: {
     "readme.md": `${ReadMe}`,
